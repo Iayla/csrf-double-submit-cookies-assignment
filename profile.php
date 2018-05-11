d160f65dc217206c53c8509d664c845c47992948
Tiffany Shanaz <tiffanyshanaz@yahoo.com>
2018-05-11 16:01:13 +0530

    initial commit

--- profile.php
+++ profile.php
@@ -106,7 +106,95 @@
       <p>  </p>
       <hr>
       <h3>Update User Profile</h3>
-      <p> </p>
+      <p>
+
+        <div class="container">
+                <div class="row" align="center" style="padding-top: 35px;">
+                <div class="col-12">
+
+                    <div class="card">
+                      <h5 class="card-header">Update Profile</h5>
+                      <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-2"></div>
+                                <div class="col-sm-8">
+
+                                    <script>
+
+                                    function getCookie(cname) {
+                                        var name = cname + "=";
+                                        var decodedCookie = decodeURIComponent(document.cookie);
+                                        var ca = decodedCookie.split(';');
+                                        for(var i = 0; i <ca.length; i++) {
+                                            var c = ca[i];
+                                            while (c.charAt(0) == ' ') {
+                                                c = c.substring(1);
+                                            }
+                                            if (c.indexOf(name) == 0) {
+                                                return c.substring(name.length, c.length);
+                                            }
+                                        }
+                                        return "";
+                                    }
+
+
+                                    function submitForm(oFormElement)
+                                    {
+                                        document.getElementById("csrf_Token").value=getCookie("csrf_token");
+
+                                    }
+                                    </script>
+
+
+
+                        <?php if(isset($_COOKIE['S_cookie'])) {
+                        echo "
+        						                <form method='post' action='csrf-validate.php' onsubmit='submitForm(this);'>
+
+                                    <!-- csrf token creation -->
+                                      <input type='hidden' name='csrf_Token' id='csrf_Token' value=''>
+                                    <!-- csrf token end  -->
+
+                                    <!-- detail form content start -->
+                                    <div class='form-group row'>
+                                    	<label for='name' class='col-sm-2 col-form-label'>Full Name</label>
+                                      <div class='col-sm-10'>
+                                        <input type='text' class='form-control' id='name' name='name' placeholder='Full Name' required>
+                                      </div>
+                                    </div>
+
+                                  	<div class='form-group row'>
+                                      <label for='university' class='col-sm-2 col-form-label'>University</label>
+                                      <div class='col-sm-10'>
+                                        <input type='text' class='form-control' id='university' name='university' placeholder='University' required>
+                                      </div>
+                                  	</div>
+
+        							              <div class='form-group row'>
+                                      <label for='degree' class='col-sm-2 col-form-label'>Degree</label>
+                                      <div class='col-sm-10'>
+                                        <input type='text' class='form-control' id='degree' name='degree' placeholder='Degree' required>
+                                      </div>
+                                  	</div>
+
+                                  	<div class='form-group row'>
+                                        <label for='year' class='col-sm-2 col-form-label'>Year</label>
+                                    <div class='col-sm-10'>
+                                        <input type='number' class='form-control' id='year' name='year' placeholder='Year' required>
+                                    </div>
+                                  	</div>
+
+                                    <button type='submit' class='btn btn-primary' >Submit</button>
+                                    <!-- form content end-->
+
+                               </form>";
+                        }
+                        ?>
+
+
+
+
+      </p>
     </div>
     <div class="col-sm-2 sidenav">
 
